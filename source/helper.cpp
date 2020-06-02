cc2058e5ff1981fe9a0dbcf10e66747078d49ba5
Lounis Bouzit <bouzitl@umich.edu>
2020-06-02 17:21:36 -0400

    Improved eye region fitting

--- source/helper.cpp
+++ source/helper.cpp
@@ -23,20 +23,39 @@ int readInput(vector<int> &face_pts, string &outputName) {
             row.push_back(word);
         }
         
-        // Eye data
-        if (stoi(row[5]) == 0 || stoi(row[6]) == 0 || stoi(row[7]) == 0 || stoi(row[8]) == 0) {
+        // Pt. 37
+        if (stoi(row[27]) == 0 || stoi(row[28]) == 0) {
+            left_center.push_back(left_center[total_frame-1]);
+            right_center.push_back(left_center[total_frame - 1]);
+        }
+        // Pt. 40
+        else if (stoi(row[33]) == 0 || stoi(row[34]) == 0) {
+            left_center.push_back(left_center[total_frame-1]);
+            right_center.push_back(left_center[total_frame - 1]);
+        }
+        // Pt. 43
+        else if (stoi(row[39]) == 0 || stoi(row[40]) == 0) {
+            left_center.push_back(left_center[total_frame-1]);
+            right_center.push_back(left_center[total_frame - 1]);
+        }
+        // Pt. 46
+        else if (stoi(row[45]) == 0 || stoi(row[46]) == 0) {
             left_center.push_back(left_center[total_frame-1]);
             right_center.push_back(left_center[total_frame - 1]);
         }
         else {
             vector<int> new_row;
-            new_row.reserve(2);
-            new_row.push_back(stoi(row[5]));
-            new_row.push_back(stoi(row[6]));
+            new_row.reserve(4);
+            new_row.push_back(stoi(row[27]));
+            new_row.push_back(stoi(row[28]));
+            new_row.push_back(stoi(row[33]));
+            new_row.push_back(stoi(row[34]));
             left_center.push_back(new_row);
             new_row.clear();
-            new_row.push_back(stoi(row[7]));
-            new_row.push_back(stoi(row[8]));
+            new_row.push_back(stoi(row[39]));
+            new_row.push_back(stoi(row[40]));
+            new_row.push_back(stoi(row[45]));
+            new_row.push_back(stoi(row[46]));
             right_center.push_back(new_row);
         }
         
@@ -56,16 +75,16 @@ int readInput(vector<int> &face_pts, string &outputName) {
 }
 
 void getPupil(int frame_num, Mat &frame_gray, ofstream &outFile) {
-    Rect leftEyeRegion(left_center[frame_num][0]-25, left_center[frame_num][1]-15, 50, 30);
-    Rect rightEyeRegion(right_center[frame_num][0]-25, right_center[frame_num][1]-15, 50, 30);
+    Rect leftEyeRegion(left_center[frame_num][0]-5, left_center[frame_num][1]-15, left_center[frame_num][2]-left_center[frame_num][0]+10, 30);
+    Rect rightEyeRegion(right_center[frame_num][0]-5, right_center[frame_num][1]-15, right_center[frame_num][2]-right_center[frame_num][0]+10, 30);
     
     Point leftPupil = findPupilCenter(frame_gray, leftEyeRegion);
     Point rightPupil = findPupilCenter(frame_gray, rightEyeRegion);
 
     // Apply shift
-    leftPupil.x += left_center[frame_num][0] - 25;
+    leftPupil.x += left_center[frame_num][0] - 5;
     leftPupil.y += left_center[frame_num][1] - 15;
-    rightPupil.x += right_center[frame_num][0] - 25;
+    rightPupil.x += right_center[frame_num][0] - 5;
     rightPupil.y += right_center[frame_num][1] - 15;
     
     outFile << leftPupil.x << "," << leftPupil.y << "," << rightPupil.x << "," << rightPupil.y << ",";
